d1e2035a2715db0f226e134307c655b7b80e7073
Peter Hallum <pmhallum@yahoo.com>
2015-01-03 13:16:29 -0500

    Updated collision handling for multiple object collisions and began experimenting with || processing

--- SomethingFromNothing.py
+++ SomethingFromNothing.py
@@ -2,6 +2,7 @@
 
 from collections import defaultdict
 
+import concurrent.futures
 import math
 import pygame
 import random
@@ -39,7 +40,7 @@ class ssobj:
                  dx     = (None, None),
                  ddx    = (None, None),
                  radius = .5,
-                 mass   = 1000):
+                 mass   = 10):
 
         lop = [x,dx,ddx]
         for idx,x in enumerate(lop):
@@ -53,55 +54,61 @@ class ssobj:
         self._density   = self._mass / ((4/3) * math.pi * (self._radius ** 3))
         self._mergelist = []
 
-        #Save next updates and flag if already calculated
-        self._nextlopsource = []
-        self._nextlop       = objlop((0,0),(0,0),(0,0))
-
+        
     #Calculate neighborhood affects of G and update acceleration
     def updateit(self,system):
+        removeList       = []
+        CollisionChkList = [self]
         
         #Calculate interactions between objects
         FTot = (0,0)
+        
+        for itemS in CollisionChkList:
+            for itemO in system._listofobjs:
+                #if item != self and not self._nextlopsource.count(item._name):
+                if not CollisionChkList.count(itemO):
+                    #Find rage and normalized directional vector from self to item
+                    FoR     = tuple(map(lambda x, y: x - y, itemO._lop._x, itemS._lop._x))
+                    Dist    = math.hypot(FoR[0],FoR[1])
+                    FoRNorm = tuple(map(lambda x: x / Dist, FoR))
+                
+                    #If items do collide - update direction of first with force of
+                    #  impact, mass, etc. and remove second from system.
+                    if Dist <= itemS._radius + itemO._radius:
+                    
+                        #Calculate additional force from impact of item
+                        self._lop._dx = tuple(map(lambda x, y: (itemO._mass * x + self._mass * y) / (itemO._mass + self._mass), itemO._lop._dx, self._lop._dx))
+                            
+                        #Update 'static' properties
+                        self._mass   += itemO._mass
+                        self._density = (self._density + itemO._density) / 2
+                        self._radius  = ( (self._density / self._mass) * ((4/3) * math.pi) ) ** (-1/3)
+                    
+                        #Add collided items to checklist
+                        CollisionChkList.append(itemO)
+
         for item in system._listofobjs:
-            if item != self and not self._nextlopsource.count(item._name):
+            #if item != self and not self._nextlopsource.count(item._name):
+            if not CollisionChkList.count(item):
                 #Find rage and normalized directional vector from self to item
                 FoR     = tuple(map(lambda x, y: x - y, item._lop._x, self._lop._x))
                 Dist    = math.hypot(FoR[0],FoR[1])
                 FoRNorm = tuple(map(lambda x: x / Dist, FoR))
-
+                
                 #If items do not collide - calculate force of gravity
                 if Dist > self._radius + item._radius:
                     FoG  = ConstG * (self._mass * item._mass) / (Dist ** 2)
-                    #self._neighbors.append([FoRx, FoRy, Dist, FoG])
                     FTot = tuple(map(lambda x, y: FoG * x + y, FoRNorm, FTot))
-
-                    item._nextlopsource.append(self._name)
-                    item._nextlop._ddx = tuple(map(lambda x, y: 
-                        -FoG * x / item._mass + y, FoRNorm, item._nextlop._ddx))
-
-                #If items do collide - update direction of first with force of
-                #  impact, mass, etc. and remove second from system.
-                else:
-                    #Calculate additional force from impact of item
-                    self._lop._dx = tuple(map(lambda x, y: 
-                        (item._mass * x + self._mass * y) / (item._mass + self._mass), item._lop._dx, self._lop._dx))
-                        
-                    #Update 'static' properties
-                    self._mass   += item._mass
-                    self._density = (self._density + item._density) / 2
-                    self._radius  = ( (self._density / self._mass) * ((4/3) * math.pi) ) ** (-1/3)
-
-                    #Remove second obj.
-                    system._listofobjs.remove(item)
                     
         #Move object and update acceleration
         self.moveit(FTot)
-        item._nextlopsource = []
-        item._nextlop._ddx = (0,0)
-
+        CollisionChkList.remove(self)
+        return CollisionChkList
+        
     #Move the object within the solar system
     def moveit(self, FTot):
-        self._lop._ddx = tuple(map(lambda x, y:    x + y, self._nextlop._ddx, FTot))
+        self._lop._ddx = FTot
+        #self._lop._ddx = tuple(map(lambda x, y:    x + y, self._nextlop._ddx, FTot))
         self._lop._dx  = tuple(map(lambda y, z:    y + z * dt, self._lop._dx, self._lop._ddx))
         self._lop._x   = tuple(map(lambda x, y, z: x + y * dt + (z * dt**2) / 2, self._lop._x, self._lop._dx, self._lop._ddx))
         
@@ -118,7 +125,7 @@ def RunGraphics():
     
     #Make Solar System           
     sol = solarsystem()
-    for x in range(0,100): #Make n objects.
+    for x in range(0,500): #Make n objects.
         p = ssobj(name = x, dx = (0, 0), ddx = (0, 0))
         sol._listofobjs.append(p)
 
@@ -138,19 +145,43 @@ def RunGraphics():
             
             if bClearScreen:
                 win.fill((0,0,0))
-                
-            for item in sol._listofobjs:
-                item.displayit(win)
-                item.updateit(sol)
             
+            mergedObjs = []
+            for item in sol._listofobjs: 
+                item.displayit(win)
+            #    try:
+            #        mergedObjs.extend(item.updateit(sol))
+            #    except TypeError:
+            #        item.updateit(sol)
+
+            with concurrent.futures.ThreadPoolExecutor(max_workers = 4) as executor:
+                for item in sol._listofobjs: 
+                    future = executor.submit(item.updateit, sol)
+                    if not mergedObjs.count(item):
+                        mergedObjs.extend(future.result())
+            #        try:
+            #            mergedObjs.extend(future.result())
+            #        except TypeError:
+            #            future.result()
+            
+            #Remove second obj.
+            #mergedObjs = [item for item in mergedObjs if item is not None]
+            try:
+                for item in mergedObjs:
+                    try:
+                        sol._listofobjs.remove(item)
+                    except ValueError:
+                        pass
+            except TypeError:
+                pass
         
             for event in pygame.event.get():
                 if event.type in [pygame.KEYDOWN, pygame.KEYUP]:
                     keysPressed[event.key] = event.type == pygame.KEYDOWN
-                    if keysPressed[pygame.K_ESCAPE] or event.type == pygame.QUIT:
-                        raise BreakOut
-                    if keysPressed[pygame.K_SPACE]:
-                        bClearScreen = not bClearScreen
+                if keysPressed[pygame.K_ESCAPE] or event.type == pygame.QUIT:
+                    raise BreakOut
+                if keysPressed[pygame.K_SPACE]:
+                    bClearScreen = not bClearScreen
     except BreakOut:                
         pygame.quit()
         
